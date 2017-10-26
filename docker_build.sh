1d7176a3f98498fc0fcda987ecc666827d9667f1
Eric Kang <erickangatms@outlook.com>
2017-10-26 08:57:54 -0700

    .

--- docker_build.sh
+++ docker_build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 dt=`date '+%Y-%m-%d_%H-%M-%S'`
-docker build . --rm -t ericskang/webapp:$dt
-docker tag ericskang/webapp:$dt ericskang/webapp:latest
+docker build . --rm -t sqlpass.azurecr.io/webapp:$dt
+docker tag sqlpass.azurecr.io/webapp:$dt sqlpass.azurecr.io/webapp:latest
 
-docker push ericskang/webapp:$dt
-docker push ericskang/webapp:latest
+docker push sqlpass.azurecr.io/webapp:$dt
+docker push sqlpass.azurecr.io/webapp:latest
 
-# docker rmi -f $(docker images -f "dangling=true" -q)
-
-# docker rmi -f ericskang/webapp:$dt
-# docker rmi -f ericskang/webapp:latest
-# docker rmi -f node:alpine
